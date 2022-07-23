9acdd2ede6120d0c90d02407508c60e86388bad7
Serlink-Innovations <serlink.business@outlook.com>
2022-07-23 09:10:27 -0400

    v1.0.1

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+addEventListener('click', function () {
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  let itemList = [];
+  itemList = document.querySelectorAll('.item');
 
-setupCounter(document.querySelector('#counter'))
+  for (let i = 0; i < itemList.length; i++) {
+    const quantity = document.querySelector('.quantity').value;
+    const unitPrice = document.querySelector('.price').value;
+
+    let value = (Number(quantity) * Number(unitPrice)).toFixed(2);
+
+    let total = value;
+
+    document.querySelector('.total').innerHTML = `$${total}`;
+
+    console.log(total)
+  }
+
+  for (let i = 0; i < itemList.length; i++) {
+    const quantity = document.querySelector('.quantity2').value;
+    const unitPrice = document.querySelector('.price2').value;
+
+    let value = (Number(quantity) * Number(unitPrice)).toFixed(2);
+
+    let total = value;
+
+    document.querySelector('.total2').innerHTML = `$${total}`;
+
+    console.log(total)
+  }
+
+  for (let i = 0; i < itemList.length; i++) {
+    const quantity = document.querySelector('.quantity3').value;
+    const unitPrice = document.querySelector('.price3').value;
+
+    let value = (Number(quantity) * Number(unitPrice)).toFixed(2);
+
+    let total = value;
+
+    document.querySelector('.total3').innerHTML = `$${total}`;
+
+    console.log(total)
+  }
+
+})
