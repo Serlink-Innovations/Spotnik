3865c2eadb499520de84da43ed56860d870ed750
Serlink-Innovations <serlink.business@outlook.com>
2022-07-23 22:38:22 -0400

    v1.0.7

--- connect.js
+++ connect.js
@@ -1,15 +1,37 @@
 const {
     MongoClient
-} = require('mongodb');
+} = require('mongodb').MongoClient;
 
 async function main() {
-    // we'll add code here soon
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://mongo:eQ4oAA62M6fdoagd@wefoods.yhrxghe.mongodb.net/test?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
-/**
- * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
- * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
- */
-const uri = "mongodb+srv://mongo:eQ4oAA62M6fdoagd@wefoods.yhrxghe.mongodb.net/test?retryWrites=true&w=majority";
+main().catch(console.error);
 
-const client = new MongoClient(uri);
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
