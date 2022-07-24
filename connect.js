99c936bb810d755ac049cb88fc40594b39154039
Serlink-Innovations <serlink.business@outlook.com>
2022-07-23 22:55:42 -0400

    Co-authored-by: GeneralFowl <GeneralFowl@users.noreply.github.com>

--- connect.js
+++ connect.js
@@ -7,7 +7,7 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://mongo:eQ4oAA62M6fdoagd@wefoods.yhrxghe.mongodb.net/test?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://mongo:eQ4oAA62M6fdoagd@wefoods.yhrxghe.mongodb.net/wefoods?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
@@ -21,17 +21,7 @@ async function main() {
     } finally {
         await client.close();
     }
+
 }
 
 main().catch(console.error);
-
-/**
- * Print the names of all available databases
- * @param {MongoClient} client A MongoClient that is connected to a cluster
- */
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
