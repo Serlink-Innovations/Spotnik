31b35be54391a91554b048a2f2ad542029af0033
Serlink-Innovations <serlink.business@outlook.com>
2022-07-25 06:46:35 -0400

    v0.0.6

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 //Connect to mongodb
-import mongoose from "mongoose";
+import mongoose from "./mongoose";
 
 const connectdb = "mongodb+srv://connectUser:<password>@wefoods.yhrxghe.mongodb.net/wefoods?retryWrites=true&w=majority";
 mongoose.connect(connectdb, {
