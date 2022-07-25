97ecde382fb058841fd91f207ff461494b26c4d4
Serlink-Innovations <serlink.business@outlook.com>
2022-07-24 22:25:30 -0400

    v1.0.9

--- connect.js
+++ connect.js
@@ -1,8 +1,10 @@
 //Connect to mongodb
 import mongoose from "mongoose";
 
-const connectdb = "mongodb+srv://mongo:4XtFOCOZXHra5RNV@wefoods.yhrxghe.mongodb.net/wefoods?retryWrites=true&w=majority";
+const connectdb = "mongodb+srv://connectUser:swYQnDKrZvM49ILl@wefoods.yhrxghe.mongodb.net/wefoods?retryWrites=true&w=majority";
 mongoose.connect(connectdb, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then((result) => console.log("Database Connected Successfully")).catch((err) => console.log(err));
+
+console.log(`${connectdb} Connected Successfully`);
