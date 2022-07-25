79fcd4f35556b2c961fa31b0c3b6a19bdffc8b7e
Serlink-Innovations <serlink.business@outlook.com>
2022-07-24 21:39:00 -0400

    v1.0.3

--- main.js
+++ main.js
@@ -1,23 +1,91 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/**addEventListener('change', function () {
 
-setupCounter(document.querySelector('#counter'))
+  let itemList = [];
+  itemList = document.getElementsByClassName('item');
+
+  let value = document.querySelector('#quantity').value * document.querySelector('#price').value;
+  let total = Number(value).toFixed(2);
+
+  let value2 = document.querySelector('#quantity2').value * document.querySelector('#price2').value;
+  let total2 = Number(value2).toFixed(2);
+
+  let value3 = document.querySelector('#quantity3').value * document.querySelector('#price3').value;
+  let total3 = Number(value3).toFixed(2);
+
+  document.querySelector('#total').innerHTML = `$${total}`;
+  document.querySelector('#total2').innerHTML = `$${total2}`;
+  document.querySelector('#total3').innerHTML = `$${total3}`;
+
+});**/
+
+addEventListener('load', function createTable() {
+
+  //Table
+  const table = document.createElement('table')
+  table.className = 'orderTable';
+
+  //Table Header
+  const tableHead = document.createElement('thead');
+  const tableRow = document.createElement('tr');
+
+  const headers = ["Name", "Description", "Quantity", "Unit Price", "Total"];
+
+  headers.forEach(headers => {
+
+    let header = document.createElement('th');
+    header.innerText = headers;
+    tableRow.append(header);
+
+  });
+
+  table.append(tableHead);
+  tableHead.append(tableRow);
+
+  document.querySelector('.content').append(table);
+
+  let products = [
+
+    {
+      Name: 'Dhal Puri',
+      Description: 'N/A',
+      Quantity: '2',
+      UnitPrice: '26'
+    },
+    {
+      Name: 'Buss Up Shot',
+      Description: 'N/A',
+      Quantity: '2',
+      UnitPrice: '26'
+    },
+    {
+      Name: 'Mango 280g',
+      Description: 'N/A',
+      Quantity: '2',
+      UnitPrice: '14'
+    },
+    {
+      Name: 'Curry Chicken',
+      Description: 'N/A',
+      Quantity: '2',
+      UnitPrice: '18'
+    }
+
+
+  ]
+
+  products.forEach(product => {
+
+    let row = document.createElement('tr');
+
+    Object.values(product).forEach(text => {
+      let cell = document.createElement('td');
+      let textNode = document.createTextNode(text);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+    })
+    table.appendChild(row);
+  });
+
+});
